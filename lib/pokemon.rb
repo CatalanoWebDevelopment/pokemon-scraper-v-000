ffa988a368baef29d541ed120e23c797cc7e810f
Parker Catalano <catalano.work@gmail.com>
2018-01-11 19:23:18 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,7 @@
 class Pokemon
 
   def initialize(id: 'default', name: 'default', type: 'default', db: 'default')
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+    puts id
   end
 
 end
