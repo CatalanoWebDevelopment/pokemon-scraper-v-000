5877bcdbf41dc696938072b0b792826c0808c699
Parker Catalano <catalano.work@gmail.com>
2018-01-11 20:59:53 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,7 @@ class Pokemon
     @@all << self
   end
 
-  def self.save(name, type, db)
+  def self.save(name, type, hp, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
